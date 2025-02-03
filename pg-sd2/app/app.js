fdc1fe5d45c027e18e125dd2f97f13a3d7c47c91
Sandy <s.kourde@gmail.com>
2025-02-03 13:18:24 +0000

    Restoring app.js file

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -12,7 +12,7 @@ const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello sandy!");
+    res.send("Hello world!");
 });
 
 // Create a route for testing the db
