c4dfb3080592439893620bbecbd286aa2962d20c
Sonja <s.nabiesade@hotmail.com>
2025-04-06 21:42:56 +0100

    Debugged homepage

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -47,15 +47,12 @@ const { Recipe } = require("./models/recipe");
 const { Category } = require("./models/category");
 
 // Define route for homepage
-<<<<<<< HEAD
 app.get("/homepage", function (req, res) {
     const recipeSql = `
-        SELECT recipe.recipe_id, recipe.title, recipe.image, recipe.description,
-               category.category_name AS cuisineType,
-               category.category_id
+        SELECT recipe.recipe_id, recipe.title, recipe.image, recipe.description, category.category_name AS cuisineType
         FROM recipe
         JOIN category ON recipe.category_id = category.category_id
-        LIMIT 6
+        LIMIT 5
     `;
 
     db.query(recipeSql)
@@ -70,17 +67,11 @@ app.get("/homepage", function (req, res) {
                 recipes: []
             });
         });
-=======
-app.get("/homepage", async function (req, res) {
-    try {
-        const recipes = await Recipe.getFeaturedRecipes();
-        res.render("homepage", { recipes: recipes });
-    } catch (err) {
-        console.error('Error fetching featured recipes:', err);
-        res.render("homepage", { recipes: [] });
-    }
->>>>>>> e6502e6890fe105238f4d1a018a877bc24dd5d9a
 });
+
+
+
+
 
 //set up multer storage
 const storage = multer.diskStorage ({
@@ -356,8 +347,8 @@ app.listen(3000,function(){
 // Test session route
 app.get("/session-test", function (req, res) {
     if (req.session.userId) {
-        res.send(`Session is active! User ID: ${req.session.userId}`);
+      res.send(`Session is active! User ID: ${req.session.userId}`);
     } else {
-        res.send("No active session. Please log in.");
+      res.send("No active session. Please log in.");
     }
-});
+  });
